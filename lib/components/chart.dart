c22df61d2ff62e22ab5f0972d237ca1d3b2ff6fc
diego <diegorodrigues-5@poli.ufrj.br>
2022-01-23 04:55:01 -0300

    :sparkles: Melhorando responsividade

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -39,28 +39,24 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.4,
-      child: Card(
-        elevation: 6,
-        margin: const EdgeInsets.all(20),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: groupedTransactions.map((tr) {
-              return Flexible(
-                fit: FlexFit.tight,
-                child: ChartBar(
-                  label: tr['day'],
-                  value: tr['value'],
-                  percentage: _weekTotalValue == 0
-                      ? 0
-                      : (tr['value'] / _weekTotalValue),
-                ),
-              );
-            }).toList(),
-          ),
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'],
+                value: tr['value'],
+                percentage:
+                    _weekTotalValue == 0 ? 0 : (tr['value'] / _weekTotalValue),
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
